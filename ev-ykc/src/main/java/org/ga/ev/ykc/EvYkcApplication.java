59dc688df08bde1090d076f842529b39e147d7ce
wanzhs <wanzhs_cedar@163.com>
2020-05-21 16:23:29 +0800

    云快充

---
+++ ev-ykc/src/main/java/org/ga/ev/ykc/EvYkcApplication.java
@@ -0,0 +1,13 @@
+package org.ga.ev.ykc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvYkcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvYkcApplication.class, args);
+    }
+
+}
---
+++ ev-ykc/src/test/java/org/ga/ev/ykc/EvYkcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ga.ev.ykc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvYkcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
